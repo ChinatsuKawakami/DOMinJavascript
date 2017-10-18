1376fc29dc705e570d091dcd67842b3500e1b56b
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-10-18 14:16:47 -0400

    Version : 0.0.5 add the event handler which shows team member's name

--- Ex_Files_DOM_JS_jQuery_Angular_React/Exercise Files/ch2/02_02/Start/js/about.js
+++ Ex_Files_DOM_JS_jQuery_Angular_React/Exercise Files/ch2/02_02/Start/js/about.js
@@ -1,4 +1,31 @@
 
+window.onload = function(){
+
+    var btnShow = document.getElementById("btnShowNames");
+    var btnHide = document.getElementById("btnHideNames");
+    var result = document.getElementById("team-member-names");
+
+    btnShow.addEventListener("click",function(){
+        var team = document.getElementsByClassName("team-name");
+         var names = "";
+         for(var i=0;i<team.length;i++){
+           names+= team[i].innerHTML + "<br>";
+         }
+         result.innerHTML = names;
+       });
+
+       btnHide.addEventListener("click",function(){
+         var result = document.getElementById("team-member-names");
+          result.innerHTML = "";
+       });
+
+}
+
+
+
+
+
+
 function effectOn(id){
     var member = document.getElementById(id);
     var memberName = member.nextElementSibling.innerHTML;
